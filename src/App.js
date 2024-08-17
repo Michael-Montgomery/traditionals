751b4980b9dee20a03bdca149126b3ac08674a30
michael-montgomery <thehealthyprogrammer@outlook.com>
2024-08-17 09:27:36 -0400

    updated browswerRouter to hashRouter for gh-pages compatibility.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import Cuts from './pages/cuts/cuts';
 import Brews from './pages/brews/brews';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/cuts' element={<Cuts></Cuts>}></Route>
         <Route path='/brews' element={<Brews></Brews>}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
